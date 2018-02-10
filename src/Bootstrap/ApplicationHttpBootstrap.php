fe1c9e25c7052f05c910d6fb050bf7f594b0cf55
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-02-10 23:21:05 +0100

    added ApplicationHttpBootstrap

---
+++ src/Bootstrap/ApplicationHttpBootstrap.php
@@ -0,0 +1,73 @@
+<?php
+namespace KiwiSuite\ApplicationHttp\Bootstrap;
+
+use KiwiSuite\Application\Bootstrap\BootstrapInterface;
+use KiwiSuite\Application\ConfiguratorItem\ConfiguratorRegistry;
+use KiwiSuite\Application\Service\ServiceRegistry;
+use KiwiSuite\ApplicationHttp\ConfiguratorItem\MiddlewareConfiguratorItem;
+use KiwiSuite\ApplicationHttp\ConfiguratorItem\PipeConfiguratorItem;
+use KiwiSuite\ApplicationHttp\Factory\FastRouterFactory;
+use KiwiSuite\ApplicationHttp\Factory\RequestHandlerRunnerFactory;
+use KiwiSuite\ApplicationHttp\Middleware\Factory\MiddlewareSubManagerFactory;
+use KiwiSuite\ApplicationHttp\Middleware\Factory\SegmentMiddlewareFactory;
+use KiwiSuite\ApplicationHttp\Middleware\MiddlewareSubManager;
+use KiwiSuite\ApplicationHttp\Middleware\SegmentMiddlewarePipe;
+use KiwiSuite\ServiceManager\ServiceManager;
+use KiwiSuite\ServiceManager\ServiceManagerConfigurator;
+use Zend\Expressive\Router\FastRouteRouter;
+use Zend\HttpHandlerRunner\RequestHandlerRunner;
+
+final class ApplicationHttpBootstrap implements BootstrapInterface
+{
+
+    /**
+     * @param ConfiguratorRegistry $configuratorRegistry
+     */
+    public function configure(ConfiguratorRegistry $configuratorRegistry): void
+    {
+        /** @var ServiceManagerConfigurator $serviceManagerConfigurator */
+        $serviceManagerConfigurator = $configuratorRegistry->getConfigurator('serviceManagerConfigurator');
+        $serviceManagerConfigurator->addFactory(RequestHandlerRunner::class, RequestHandlerRunnerFactory::class);
+        $serviceManagerConfigurator->addFactory(FastRouteRouter::class, FastRouterFactory::class);
+        $serviceManagerConfigurator->addSubManager(MiddlewareSubManager::class, MiddlewareSubManagerFactory::class);
+
+        /** @var ServiceManagerConfigurator $middlewareConfigurator */
+        $middlewareConfigurator = $configuratorRegistry->getConfigurator('middlewareConfigurator');
+        $middlewareConfigurator->addFactory(SegmentMiddlewarePipe::class, SegmentMiddlewareFactory::class);
+    }
+
+    /**
+     * @param ServiceRegistry $serviceRegistry
+     */
+    public function addServices(ServiceRegistry $serviceRegistry): void
+    {
+        // TODO: Implement addServices() method.
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getConfiguratorItems(): ?array
+    {
+        return [
+            MiddlewareConfiguratorItem::class,
+            PipeConfiguratorItem::class
+        ];
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getDefaultConfig(): ?array
+    {
+        // TODO: Implement getDefaultConfig() method.
+    }
+
+    /**
+     * @param ServiceManager $serviceManager
+     */
+    public function boot(ServiceManager $serviceManager): void
+    {
+        // TODO: Implement boot() method.
+    }
+}
--- src/HttpApplication.php
+++ src/HttpApplication.php
@@ -11,25 +11,11 @@
 declare(strict_types=1);
 namespace KiwiSuite\ApplicationHttp;
 
-use KiwiSuite\Application\ApplicationConfigurator;
 use KiwiSuite\Application\ApplicationInterface;
 use KiwiSuite\Application\Bootstrap;
-use KiwiSuite\Application\ConfiguratorItem\ConfiguratorRegistry;
-use KiwiSuite\ApplicationHttp\ConfiguratorItem\MiddlewareConfiguratorItem;
-use KiwiSuite\ApplicationHttp\ConfiguratorItem\PipeConfiguratorItem;
-use KiwiSuite\ApplicationHttp\ConfiguratorItem\RouteConfiguratorItem;
-use KiwiSuite\ApplicationHttp\Factory\ApplicationFactory;
-use KiwiSuite\ApplicationHttp\Middleware\Factory\MiddlewareSubManagerFactory;
-use KiwiSuite\ApplicationHttp\Middleware\MiddlewareSubManager;
 use KiwiSuite\ApplicationHttp\Pipe\PipeConfig;
-use KiwiSuite\ApplicationHttp\Route\RouteConfig;
-use KiwiSuite\CommonTypes\Bootstrap\CommonTypesBootstrap;
-use KiwiSuite\Database\Bootstrap\DatabaseBootstrap;
-use KiwiSuite\Entity\Bootstrap\EntityBootstrap;
-use KiwiSuite\ProjectUri\Bootstrap\ProjectUriBootstrap;
 use KiwiSuite\ServiceManager\ServiceManager;
-use KiwiSuite\ServiceManager\ServiceManagerConfigurator;
-use Zend\Expressive\Application;
+use Zend\HttpHandlerRunner\RequestHandlerRunner;
 
 final class HttpApplication implements ApplicationInterface
 {
@@ -54,37 +40,9 @@ final class HttpApplication implements ApplicationInterface
     {
         /** @var ServiceManager $serviceManager */
         $serviceManager = (new Bootstrap())->bootstrap($this->bootstrapDirectory, $this);
-        ($serviceManager->build(Application::class, [
+
+        ($serviceManager->build(RequestHandlerRunner::class, [
             PipeConfig::class => $serviceManager->get(PipeConfig::class),
-            RouteConfig::class => $serviceManager->get(RouteConfig::class),
         ]))->run();
-    }
-
-    /**
-     * @param ApplicationConfigurator $applicationConfigurator
-     * @return mixed
-     */
-    public function configureApplicationConfig(ApplicationConfigurator $applicationConfigurator) : void
-    {
-        $applicationConfigurator->addConfiguratorItem(MiddlewareConfiguratorItem::class);
-        $applicationConfigurator->addConfiguratorItem(PipeConfiguratorItem::class);
-        $applicationConfigurator->addConfiguratorItem(RouteConfiguratorItem::class);
-
-        $applicationConfigurator->addBootstrapItem(EntityBootstrap::class);
-        $applicationConfigurator->addBootstrapItem(CommonTypesBootstrap::class);
-        $applicationConfigurator->addBootstrapItem(ProjectUriBootstrap::class);
-        $applicationConfigurator->addBootstrapItem(DatabaseBootstrap::class);
-    }
-
-    /**
-     * @param ConfiguratorRegistry $configuratorRegistry
-     */
-    public function configure(ConfiguratorRegistry $configuratorRegistry): void
-    {
-        /** @var ServiceManagerConfigurator $serviceManagerConfigurator */
-        $serviceManagerConfigurator = $configuratorRegistry->getConfigurator('serviceManagerConfigurator');
-
-        $serviceManagerConfigurator->addFactory(Application::class, ApplicationFactory::class);
-        $serviceManagerConfigurator->addSubManager(MiddlewareSubManager::class, MiddlewareSubManagerFactory::class);
     }
 }
