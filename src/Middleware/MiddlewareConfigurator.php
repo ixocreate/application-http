8225c99e56ddce3ee91cb55657f99c00e642d948
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-15 15:14:24 +0100

    CS

--- src/Middleware/MiddlewareConfigurator.php
+++ src/Middleware/MiddlewareConfigurator.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * kiwi-suite/application-http (https://github.com/kiwi-suite/application-http)
+ *
+ * @package kiwi-suite/application-http
+ * @see https://github.com/kiwi-suite/application-http
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
 namespace KiwiSuite\ApplicationHttp\Middleware;
 
 use KiwiSuite\Contract\Application\ConfiguratorInterface;
